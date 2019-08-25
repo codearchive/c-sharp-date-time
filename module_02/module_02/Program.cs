80b420af9c142c7f32ec2324d2165dc6afd204e6
Pavel Patrusov <pavelp9415@gmail.com>
2019-08-25 21:53:17 +0300

    Completed unit 2.1

---
+++ module_02/module_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"StockData.csv");
+
+            foreach (var line in lines.Skip(1))
+            {
+                var segmants = line.Split(',');
+
+                var tradeDate = DateTime.Parse(segmants[1], CultureInfo.GetCultureInfo("us-US"));
+
+                Console.WriteLine(tradeDate.ToLongDateString());
+            }
+        }
+    }
+}

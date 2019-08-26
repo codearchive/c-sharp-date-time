39de3164f771f2d9e28e0e595ea26a9d5c779b96
Pavel Patrusov <pavelp9415@gmail.com>
2019-08-26 21:06:55 +0300

    Completed unit 3.1

---
+++ module_03/module_03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace module_03
+{
+    class Program
+    {
+        static void Main()
+        {
+            var start = DateTimeOffset.UtcNow;
+            var end = start.AddSeconds(45);
+            TimeSpan difference = end - start;
+
+            Console.WriteLine(difference.Seconds);
+        }
+    }
+}

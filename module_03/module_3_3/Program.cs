82e1d8cff54428597f9b19ff2dc98cb63ad889b2
Pavel Patrusov <pavelp9415@gmail.com>
2019-08-27 21:47:58 +0300

    Completed unit 3.3

---
+++ module_03/module_3_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contractDate = new DateTimeOffset(2020, 2, 29, 0, 0, 0, TimeSpan.Zero);
+
+            Console.WriteLine(contractDate);
+
+            contractDate = ExtendContract(contractDate, 1);
+
+            Console.WriteLine(contractDate);
+        }
+
+        private static DateTimeOffset ExtendContract(DateTimeOffset current, int months)
+        {
+            var newContractDate = current.AddMonths(months).AddTicks(-1);
+
+            return new DateTimeOffset(newContractDate.Year, newContractDate.Month, DateTime.DaysInMonth(newContractDate.Year, newContractDate.Month), 23, 59, 59, current.Offset);
+        }
+    }
+}
